df9b427e5604689f17c8d8ffa6603d0a6398923a
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-12-04 20:38:45 +0200

    1

--- src/controllers/lists/products/ProductListController.vue
+++ src/controllers/lists/products/ProductListController.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
-    <!--<pre>{{ filterParams }}</pre>-->
-    <!--<pre>{{ filters }}</pre>-->
-    <!--<pre>{{ filterLookupNames }}</pre>-->
     <FilterBreadcrumbs
-      :filterParams="filterParams"
+      urlParamName="filterParams"
+      v-model:filters="filters"
+      v-model:filterLookupNames="filterLookupNames"
     />
     <productList
       :parentKeyValuePair="parentKeyValuePair"
@@ -12,6 +11,8 @@
       v-model:filters="filters"
       v-model:filterLookupNames="filterLookupNames"
     />
+    <!--<pre>{{filters}}</pre>-->
+    <!--<pre>{{filterLookupNames}}</pre>-->
   </div>
 </template>
 
@@ -31,81 +32,7 @@ export default {
       fetchFlags: {},
       filters: {}, // Stores filter IDs
       filterLookupNames: {}, // Stores filter names
-      filterParams: [], // Array format used for route parameter
     };
-  },
-  methods: {
-    // Decode filterParams (array) to populate filters and filterLookupNames
-    filterParamsDecode(filterParams) {
-      const newFilters = {};
-      const newFilterLookupNames = {};
-
-      for (let i = 0; i < filterParams.length; i += 3) {
-        const key = filterParams[i];
-        newFilters[key] = parseInt(filterParams[i + 1], 10) || 0;
-        newFilterLookupNames[key] = filterParams[i + 2] || "All";
-      }
-
-      this.filters = newFilters;
-      this.filterLookupNames = newFilterLookupNames;
-    },
-    // Encode filters and filterLookupNames into filterParams (array)
-    filterParamsEncode() {
-      const params = [];
-      let lastNonDefaultIndex = -1;
-
-      // Build params and track the last non-default filter set
-      Object.keys(this.filterLookupNames).forEach((key, index) => {
-        const id = this.filters[key] !== null ? this.filters[key] : 0;
-        const name = this.filterLookupNames[key] !== null ? this.filterLookupNames[key] : "All";
-
-        params.push(key, id, name);
-
-        // Check if this is a non-default filter
-        if (id !== 0 || name !== "All") {
-          lastNonDefaultIndex = index;
-        }
-      });
-
-      // If all are default, return an empty array
-      if (lastNonDefaultIndex === -1) {
-        return [];
-      }
-
-      // Trim trailing default filter sets
-      return params.slice(0, (lastNonDefaultIndex + 1) * 3); // Multiply by 3 to include full sets
-    },
-
-    // Update filterParams and route
-    updateRoute() {
-      this.filterParams = this.filterParamsEncode();
-      this.$router.push({
-        name: "/lists/products/filter/:filterParams*",
-        params: { filterParams: this.filterParams },
-      });
-    },
-  },
-  watch: {
-    // Watch filters and filterLookupNames for changes
-    filters: {
-      deep: true,
-      handler() {
-        this.updateRoute();
-      },
-    },
-    filterLookupNames: {
-      deep: true,
-      handler() {
-        this.updateRoute();
-      },
-    },
-  },
-  created() {
-    // On page load, decode filterParams from route
-    const initialFilterParams = this.$route.params.filterParams || [];
-    if (initialFilterParams.length) {
-      this.filterParamsDecode(initialFilterParams);
-    }
   },
 };
 </script>
--- src/views/FilterBreadcrumbs.vue
+++ src/views/FilterBreadcrumbs.vue
@@ -8,6 +8,10 @@
         class="cursor-pointer"
       />
     </q-breadcrumbs>
+    <!--<pre>{{ filters }}</pre>-->
+    <!--<pre>{{ filterLookupNames }}</pre>-->
+    <!--<pre>{{ filterParams }}</pre>-->
+    <!--<pre>{{ urlParamName }}</pre>-->
   </div>
 </template>
 <script>
@@ -15,10 +19,29 @@
 export default {
   name: "FilterBreadcrumbs",
   props: {
-    filterParams: {
-      type: Array,
-      required: true,
+    urlParamName: {
+      type: String,
+      default: () => {
+        return ""
+      }
     },
+    filters: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    },
+    filterLookupNames: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    },
+  },
+  data(){
+    return {
+      filterParams: []
+    }
   },
   computed: {
 
@@ -43,8 +66,89 @@ export default {
       return trail;
     },
   },
-  methods: {
 
+  methods: {
+    // Decode filterParams (array) to populate filters and filterLookupNames
+    filterParamsDecode(filterParams) {
+      const newFilters = {};
+      const newFilterLookupNames = {};
+
+      for (let i = 0; i < filterParams.length; i += 3) {
+        const key = filterParams[i];
+        newFilters[key] = parseInt(filterParams[i + 1], 10) || 0;
+        newFilterLookupNames[key] = filterParams[i + 2] || "All";
+      }
+
+      return {
+        newFilters,
+        newFilterLookupNames
+      }
+    },
+    // Encode filters and filterLookupNames into filterParams (array)
+    filterParamsEncode() {
+      const params = [];
+      let lastNonDefaultIndex = -1;
+
+      // Build params and track the last non-default filter set
+      Object.keys(this.filterLookupNames).forEach((key, index) => {
+        const id = this.filters[key] !== null ? this.filters[key] : 0;
+        const name = this.filterLookupNames[key] !== null ? this.filterLookupNames[key] : "All";
+
+        params.push(key, id, name);
+
+        // Check if this is a non-default filter
+        if (id !== 0 || name !== "All") {
+          lastNonDefaultIndex = index;
+        }
+      });
+
+      // If all are default, return an empty array
+      if (lastNonDefaultIndex === -1) {
+        return [];
+      }
+
+      // Trim trailing default filter sets
+      return params.slice(0, (lastNonDefaultIndex + 1) * 3); // Multiply by 3 to include full sets
+    },
+
+    // Update filterParams and route
+    updateRoute() {
+      this.filterParams = this.filterParamsEncode();
+      this.$router.push({
+        name: this.$route.name, // Stay on the current route
+        params: {
+          ...this.$route.params, // Keep existing params
+          [this.urlParamName]: this.filterParams, // Dynamically set the parameter name
+        },
+      });
+    },
+  },
+  watch: {
+    // Watch filters and filterLookupNames for changes
+    filters: {
+      deep: true,
+      handler() {
+        this.updateRoute();
+      },
+    },
+    // filterLookupNames: {
+    //   deep: true,
+    //   handler() {
+    //     this.updateRoute();
+    //   },
+    // },
+  },
+  mounted() {
+    // On page load, decode filterParams from route
+    const initialFilterParams = this.$route.params[this.urlParamName] || [];
+    if (initialFilterParams.length) {
+      const filterParamsDecode = this.filterParamsDecode(initialFilterParams);
+
+      this.$emit('update:filters', filterParamsDecode.newFilters)
+      this.$emit('update:filterLookupNames', filterParamsDecode.newFilterLookupNames)
+      // console.log('filterParams1')
+      // console.log(this.filterParams)
+    }
   },
 };
 </script>
