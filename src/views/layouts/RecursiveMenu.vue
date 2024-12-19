cd1cde64da68997c286f64d64e8d9faefa07f6b4
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-12-19 13:53:40 +0200

    1

--- src/views/layouts/RecursiveMenu.vue
+++ src/views/layouts/RecursiveMenu.vue
@@ -1,13 +1,58 @@
-<script>
-export default {
-  name: "RecursiveMenu"
-}
-</script>
-
 <template>
-
+  <div>
+    <q-btn
+      v-for="(main, index) in menuList"
+      :key="main.uuid"
+      flat
+      color="grey-1"
+      class="header-toolbar-title q-mx-xs"
+      :label="main.func"
+      @mouseover="mouseOverMenuEvt(main)"
+      @mouseleave="mouseLeaveMenuEvt(main)"
+    >
+      <q-menu
+        :ref="main.uuid"
+        content-class="bg-grey-4 text-brown-7 header-nav-title"
+        transition-show="jump-down"
+        transition-hide="jump-up"
+      >
+        <q-list>
+          <template v-for="(child, idx) in main.children" :key="child.uuid">
+            <RecursiveMenuItem :node="child" @mouse-over="mouseOverMenuEvt" @mouse-leave="mouseLeaveMenuEvt" />
+          </template>
+        </q-list>
+      </q-menu>
+    </q-btn>
+  </div>
 </template>
 
-<style scoped>
+<script>
+import RecursiveMenuItem from "./RecursiveMenuItem.vue";
 
+export default {
+  name: "RecursiveMenu",
+  components: {
+    RecursiveMenuItem,
+  },
+  props: {
+    menuList: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    mouseOverMenuEvt(node) {
+      this.$emit("mouse-over", node);
+    },
+    mouseLeaveMenuEvt(node) {
+      this.$emit("mouse-leave", node);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.header-toolbar-title {
+  cursor: pointer;
+}
 </style>
--- src/views/layouts/RecursiveMenuItem.vue
+++ src/views/layouts/RecursiveMenuItem.vue
@@ -1,13 +1,54 @@
-<script>
-export default {
-  name: "RecursiveMenuItem"
-}
-</script>
-
 <template>
-
+  <q-item
+    :class="{ 'q-item-active': node.children?.length > 0 }"
+    clickable
+    @mouseover="handleMouseOver"
+    @mouseleave="handleMouseLeave"
+  >
+    <q-item-section>{{ node.func }}</q-item-section>
+    <q-item-section v-if="node.children?.length > 0" side>
+      <q-icon name="keyboard_arrow_right" />
+    </q-item-section>
+    <q-menu
+      v-if="node.children?.length > 0"
+      :ref="node.uuid"
+      anchor="top end"
+      self="top start"
+      content-class="bg-grey-4 text-brown-7 header-nav-title"
+      transition-show="scale"
+      transition-hide="scale"
+    >
+      <q-list>
+        <template v-for="(child, idx) in node.children" :key="child.uuid">
+          <RecursiveMenuItem :node="child" @mouse-over="handleMouseOver" @mouse-leave="handleMouseLeave" />
+        </template>
+      </q-list>
+    </q-menu>
+  </q-item>
 </template>
 
-<style scoped>
+<script>
+export default {
+  name: "RecursiveMenuItem",
+  props: {
+    node: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    handleMouseOver() {
+      this.$emit("mouse-over", this.node);
+    },
+    handleMouseLeave() {
+      this.$emit("mouse-leave", this.node);
+    },
+  },
+};
+</script>
 
+<style scoped>
+.q-item-active {
+  background-color: var(--q-color-grey-2);
+}
 </style>
